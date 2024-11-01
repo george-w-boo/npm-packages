18e7ed20b7f8d34fe6daa17d7b39112aadd40237
Mykhailo Ostapysko <mp4clips@gmail.com>
2024-11-01 17:41:52 +0200

    adds night greeting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { greetInTheMorning } from "./morningGreet";
 export { greetInTheEvening } from "./eveningGreet";
 export { greetInTheAfternoon } from "./afternoonGreet";
+export { greetInTheNight } from "./nightGreet";
